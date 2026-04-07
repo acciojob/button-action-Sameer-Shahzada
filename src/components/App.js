42593161b6cb4a359acab12f147f2cba565035b8
Sameer-Shahzada <md.shahzada1606@gmail.com>
2026-04-07 23:44:39 +0530

    button-action

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import './../styles/App.css';
 
+
 const App = (props) => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow(!false)
+  }
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p className={show ? "show" : "hide"} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={handleClick}>{show ? "Hide" : "Show"}</button>
     </div>
   );
 }
